dd34f1f965dacfa491c67bedbf39c2a621cc1a75
Dmitry <dmdm1717@mail.ru>
2022-10-14 21:27:44 +0300

    Add Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 13:
+//  Напишите программу, которая выводит третью цифру заданного числа 
+//  или сообщает, что третьей цифры нет. 
+//  Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+// int number_2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+// Console.WriteLine("Введите число:");
+// int number = Convert.ToInt32(Console.ReadLine());
+// {
+//     while (number < 99)
+//    Console.WriteLine($"Третьей цифры в заданном вами числе нет =(");
+//    number = Convert.ToInt32(Console.ReadLine());
+// }
+// {
+// number = Convert.ToInt32(Console.ReadLine());
+
+//     while (number > 999);
+    
+//     int number_2 = number / 10;
+    
+//     number = Convert.ToInt32(Console.ReadLine());
+// }
+//     {
+// while (number < 999 || number > 99);
+// int number = number % 10;
+//     }
+// Console.WriteLine($"Третья цифра этого числа: {number}");
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+ 
+if (number < 100)
+   Console.WriteLine($"Третьей цифры в заданном вами числе нет =(");
+else 
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    int number_3 = number % 10;
+Console.WriteLine($"Третья цифра этого числа: {number_3}");
+}
