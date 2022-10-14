fdd948123666cc326ccc89793b3f0cd787b94bd6
Dmitry <dmdm1717@mail.ru>
2022-10-14 22:08:42 +0300

    Add Task15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 15: 
+// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и 
+// проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+// while (number < 1 || number > 7)
+// {
+//     Console.WriteLine($"Такого дня недели не существует =(");
+//     number = Convert.ToInt32(Console.ReadLine());
+// }
+// {
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+switch (number)
+{
+ case1:
+Console.WriteLine("Понедельник");
+brake;
+case2:
+Console.WriteLine("Вторник");
+brake;
+case3:
+Console.WriteLine("Среда");
+brake;
+case4:
+Console.WriteLine("Четверг";)
+brake;
+case5:
+Console.WriteLine("Пятница");
+brake;
+case6:
+Console.WriteLine("Суббота - Выходной");
+brake;
+case7:
+Console.WriteLine("Воскресенье - выходной");
+brake;
+default:
+Console.WriteLine("Нет такого дня недели");
+brake;
+}
